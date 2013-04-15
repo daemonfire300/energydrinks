9086de7e949e76ef7354c2a297d5e27f64b51f41
Julius Foitzik <info@accountr.eu>
2013-04-15 23:02:31 +0200

    Removed trailing whitespaces

--- drinks/management/commands/loaddrinks.py
+++ drinks/management/commands/loaddrinks.py
@@ -21,6 +21,3 @@ class Command(BaseCommand):
         time_end = time.time()
         time_diff = time_end - time_start
         print "%s seconds" % time_diff
-            
-                
-                
