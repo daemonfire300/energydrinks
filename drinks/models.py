9dccbebf46fb2781d39f607b4409f85cd638c4d0
Julius Foitzik <info@accountr.eu>
2013-04-15 13:16:13 +0200

    Changed upload dir

--- drinks/models.py
+++ drinks/models.py
@@ -7,8 +7,8 @@ class Drink(models.Model):
     name = models.CharField(max_length=128, default="unknown drink")
     volume = models.IntegerField(default=0)
     caffeine = models.IntegerField(default=0)
-    #image = models.ImageField(upload_to=os.path.join(os.path.dirname(__file__), '../static/images').replace('\\','/'))
-    image = models.ImageField(upload_to="/home/dotcloud/data/media/")
+    image = models.ImageField(upload_to=os.path.join(os.path.dirname(__file__), '../static/images').replace('\\','/'))
+    #image = models.ImageField(upload_to="/home/dotcloud/data/media/")
     
     def __unicode__(self):
         return "%s %s ml %s mg" % (self.name, self.volume, self.caffeine)
