efd478c9b25c5fea5aa614ecd2111d574f1b35fa
Julius Foitzik <info@accountr.eu>
2013-04-17 19:02:11 +0200

    ...

--- energy/settings.py
+++ energy/settings.py
@@ -59,7 +59,8 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -70,6 +71,7 @@ STATIC_ROOT = '' #os.path.join(os.path.dirname(__file__), '../static').replace('
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
