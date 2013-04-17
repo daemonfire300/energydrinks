50225ae667f745f9b8fc5213b164fd454e3e59f7
Julius Foitzik <info@accountr.eu>
2013-04-17 16:03:49 +0200

    small deploy

--- drinks/models.py
+++ drinks/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
-
 class Drink(models.Model):
     name = models.CharField(max_length=128, default="unknown drink")
     volume = models.IntegerField(default=0)
